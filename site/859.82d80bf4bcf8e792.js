dab455d042e52bca7684f6c86ca26366aeac75cc
lap <flavioallcca@gmail.com>
2023-01-14 14:06:55 -0500

    add frontend

---
+++ site/859.82d80bf4bcf8e792.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfuse=self.webpackChunkfuse||[]).push([[859],{9859:(x,f,s)=>{s.d(f,{Fk:()=>q});var t=s(3668),d=s(6731);s(7497),s(348),s(5351),s(9133),s(9555);let q=(()=>{class a{}return a.\u0275fac=function(e){return new(e||a)},a.\u0275mod=t.oAB({type:a}),a.\u0275inj=t.cJS({imports:[[d.si,d.BQ],d.BQ]}),a})()}}]);
